4c2373c6571ebbfde1963297b8ad5c101c0ee0f2
Mariya <mariaomware@gmail.com>
2024-04-09 23:03:29 +0300

    Complete Search Section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+// Loads DOM content
+document.addEventListener('DOMContentLoaded', function () {
+    // Call display recipes to display when page loads
+    displayRecipes();
+    // Assign the elements to variables
+    const form = document.getElementById('search-form');
+    const input = document.getElementById('search-input');
+
+    // Add an event listener for the submission
+    form.addEventListener('submit', function (event) {
+    // Prevent the submission form from returning to default
+      event.preventDefault();
+    // Get the value entered in the search bar
+      let searchTerm = input.ariaValueMax.trim();
+    // Check if the seach input is empty
+      if (searchTerm !== '') {
+    // Search for recipes using specific words 
+    searchRecipes(searchTerm);
+      } else {
+        alert('Please enter valid search');
+      }
+    });
+});
 
